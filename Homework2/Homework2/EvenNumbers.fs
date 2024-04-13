feb8eef90890f43922991e68a5d0e1c48e428dd9
Nikita Pisarev <nikgooder@gmail.com>
2024-04-13 07:08:54 +0300

    Done even numbers

---
+++ Homework2/Homework2/EvenNumbers.fs
@@ -0,0 +1,10 @@
+﻿module Homework2.EvenNumbers
+
+let countEvenNumbersMap list =
+    list |> List.map (fun number -> if abs number % 2 = 0 then 1 else 0) |> List.sum
+
+let countEvenNumbersFilter list =
+    list |> List.filter (fun number -> abs number % 2 = 0) |> List.length
+
+let countEvenNumbersFold list =
+    list |> List.fold (fun acc number -> if abs number % 2 = 0 then acc + 1 else acc) 0
