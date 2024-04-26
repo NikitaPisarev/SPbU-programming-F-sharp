f7674a96bace507e45552ff0335d9f73e35da07e
Nikita Pisarev <nikgooder@gmail.com>
2024-04-26 16:11:58 +0200

    test: tests for priority queue

--- Test/Test.Tests/Tests.fs
+++ Test/Test.Tests/Tests.fs
@@ -1,8 +1,10 @@
 module Test.Tests
 
+open System
 open NUnit.Framework
 open FsUnit
 open Test.Fibonacci
+open Test.PriorityQueue
 
 [<TestFixture>]
 type EvenFibonacciSumTests() =
@@ -16,3 +18,27 @@ type EvenFibonacciSumTests() =
     member this.``EvenFibonacciSum returns correct sum for inputs less than 2``() =
         evenFibonacciSum 1 |> should equal 0
         evenFibonacciSum 0 |> should equal 0
+
+
+[<TestFixture>]
+type PriorityQueueTests() =
+    [<Test>]
+    member this.``Enqueue and dequeue single element``() =
+        let pq = createPriorityQueue()
+        enqueue pq 1 1
+        pq |> dequeue |> should equal 1
+
+    [<Test>]
+    member this.``Enqueue and dequeue multiple elements``() =
+        let pq = createPriorityQueue()
+        enqueue pq "High" 10
+        enqueue pq "Low" 1
+        enqueue pq "Medium" 5
+        pq |> dequeue |> should equal "Low"
+        pq |> dequeue |> should equal "Medium"
+        pq |> dequeue |> should equal "High"
+
+    [<Test>]
+    member this.``Dequeue from empty queue throws exception``() =
+        let pq = createPriorityQueue()
+        (fun () -> pq |> dequeue) |> should throw typeof<InvalidOperationException>
