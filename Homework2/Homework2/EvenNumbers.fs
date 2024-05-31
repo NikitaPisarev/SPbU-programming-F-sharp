c83baf00230de6c813d067b2de7513595750a24e
Nikita Pisarev <nikgooder@gmail.com>
2024-05-31 22:01:37 +0200

    fix: remove extra module

--- Homework2/Homework2/EvenNumbers.fs
+++ Homework2/Homework2/EvenNumbers.fs
@@ -4,7 +4,7 @@ let countEvenNumbersMap list =
     list |> List.map (fun number -> if number % 2 = 0 then 1 else 0) |> List.sum
 
 let countEvenNumbersFilter list =
-    list |> List.filter (fun number -> abs number % 2 = 0) |> List.length
+    list |> List.filter (fun number -> number % 2 = 0) |> List.length
 
 let countEvenNumbersFold list =
-    list |> List.fold (fun acc number -> if abs number % 2 = 0 then acc + 1 else acc) 0
+    list |> List.fold (fun acc number -> if number % 2 = 0 then acc + 1 else acc) 0
