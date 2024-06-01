2ff78e30d33019d0518d4d64b862d087b67ec443
Nikita Pisarev <nikgooder@gmail.com>
2024-06-01 11:22:14 +0200

    fix: delete mutable stack

--- Homework4/BracketSequence/Program.fs
+++ Homework4/BracketSequence/Program.fs
@@ -1,37 +1,25 @@
 ﻿module BracketSequence
 
-open System.Collections.Generic
+let bracets = [ ('(', ')'); ('[', ']'); ('{', '}') ]
 
-let isBalanced (s: string) : bool =
-    let matchingBracket =
-        function
-        | ')' -> Some '('
-        | ']' -> Some '['
-        | '}' -> Some '{'
-        | _ -> None
+let isOpeningBracket x =
+    bracets |> List.exists (fun (o, _) -> o = x)
 
-    let isOpening =
-        function
-        | '('
-        | '['
-        | '{' -> true
-        | _ -> false
+let isClosingBracket x =
+    bracets |> List.exists (fun (_, c) -> c = x)
 
-    let stack = Stack<char>()
-    let chars = s.ToCharArray()
+let isBracetsPair x y = List.contains (x, y) bracets
 
-    let processChar c =
-        match matchingBracket c with
-        | Some expectedOpen ->
-            if stack.Count > 0 && stack.Peek() = expectedOpen then
-                stack.Pop() |> ignore
-                true
-            else
-                false
-        | None ->
-            if isOpening c then
-                stack.Push(c)
+let isBalanced (str: string) =
+    let rec helper (stack, seq) =
+        match seq with
+        | [] -> List.isEmpty stack
+        | h :: seqTail when isOpeningBracket h -> helper (h :: stack, seqTail)
+        | h :: seqTail when isClosingBracket h ->
+            match stack with
+            | [] -> false
+            | stackHead :: stackTail when isBracetsPair stackHead h -> helper (stackTail, seqTail)
+            | _ -> false
+        | _ :: seqTail -> helper (stack, seqTail)
 
-            true
-
-    chars |> Array.forall processChar && stack.Count = 0
+    helper ([], List.ofSeq str)
